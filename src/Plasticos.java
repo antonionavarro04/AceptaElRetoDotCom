e5a75d874193f941ca758093b67546b0a58e24fa
Navarro <antonio.navarro@iesnervion.es>
2023-05-30 10:14:51 +0200

    Reduced

--- src/Plasticos.java
+++ src/Plasticos.java
@@ -1,15 +1,1 @@
-public class Plasticos {
-    static java.util.Scanner s = new java.util.Scanner(System.in);
-
-    private static int pesoPlastico(int n, int t) {
-        return t - n;
-    }
-
-    public static void main(String[] args) {
-        int c = s.nextInt();
-
-        for (int i = 0; i < c; i++) {
-            System.out.println(pesoPlastico(s.nextInt(), s.nextInt()));
-        }
-    }
-}
+public class Plasticos{static java.util.Scanner s=new java.util.Scanner(System.in);static int f(int n,int t){return t-n;}public static void main(String[]args){int c=s.nextInt();for(int i=0;i<c;i++)System.out.println(f(s.nextInt(),s.nextInt()));}}
