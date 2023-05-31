c2d9aaea5823ce983a29dfea35fc7b02aa42c01d
Navarro <antonio.navarro@iesnervion.es>
2023-05-31 09:26:56 +0200

    Compression

--- src/c++/Plasticos532.cpp
+++ src/c++/Plasticos532.cpp
@@ -1,16 +1,2 @@
 #include <iostream>
-
-int pesoPlastico(int n, int t) {
-    return t - n;
-}
-
-int main() {
-    int c;
-    std::cin >> c;
-
-    for (int i = 0; i < c; i++) {
-        int n, t;
-        std::cin >> n >> t;
-        std::cout << pesoPlastico(n, t) << std::endl;
-    } return 0;
-}
+int main(){int c;std::cin>>c;for(int i=0;i<c;i++){int n,t;std::cin>>n>>t;std::cout<<t-n<<std::endl;}return 0;}
